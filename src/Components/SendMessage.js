17a875dc6d94e11b5893bcf0bda451ec7fd31754
kangeline <kangeline@lacritz.com>
2021-11-28 18:07:29 +0100

    updated

--- src/Components/SendMessage.js
+++ src/Components/SendMessage.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { useState } from "react";
 import "../styles/App.css";
-const SendMessage = ({change}) => {
+const SendMessage = ({ change }) => {
   const [inputMessage, setInputMessage] = useState("");
-  const sendMessage =()=>{
-    console.log("inside send message",inputMessage);
-    if(inputMessage!=="")
-    {
-      console.log("inside if send message");
-    const postMessage = {message:inputMessage,author:"you"};
-    fetch("https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/", {
-      body: JSON.stringify(postMessage),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Token: "FGvviHC9KSai"
-      },
-      method: "POST"
-    })
-      .then(
-        (result) => {
-         console.log("result",result);
-         setInputMessage('');
-        },
-        (error) => {
-          console.log("error",error);
-        }
-      )
-      setInputMessage('');
-      change();
-  }
-};
+  // send Data to API
+  const sendMessage = async () => {
+    try {
+      console.log("inside send message", inputMessage);
+      if (inputMessage !== "") {
+        console.log("inside if send message");
+        const postMessage = { message: inputMessage, author: "you" };
+        const response = await fetch(
+          "https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/",
+          {
+            body: JSON.stringify(postMessage),
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Token: "FGvviHC9KSai",
+            },
+            method: "POST",
+          }
+        );
+        console.log("response", response.status);
+        setInputMessage("");
+        change();
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
   return (
     <div className="chat-footerhead">
       <div className="chat-footer">
